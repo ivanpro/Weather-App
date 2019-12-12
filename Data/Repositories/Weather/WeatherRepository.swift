fcdede112aa40488dee6adfd001288b206a33cd6
Vitor Lopes Leonardi <vitor.leonardi@theiconic.com.au>
2019-12-12 23:22:02 +1100

    Add WeatherRepository barebone

---
+++ Data/Repositories/Weather/WeatherRepository.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherRepository.swift
+//  WeatherApp
+//
+//  Created by Vitor Leonardi on 12/12/19.
+//  Copyright © 2019 Vitor. All rights reserved.
+//
+
+import Foundation
+
+protocol WeatherRepositoryInterface {}
+
+final class WeatherRepository: WeatherRepositoryInterface {}
