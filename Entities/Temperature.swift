0b5798336554dc506c5d45ce3b7a5946fb07fcbd
Vitor Lopes Leonardi <vitor.leonardi@theiconic.com.au>
2019-12-13 14:48:26 +1100

    Implement Temperature entity
    This is responsible to parse details about the weather

---
+++ Entities/Temperature.swift
@@ -0,0 +1,35 @@
+//
+//  Weather.swift
+//  WeatherApp
+//
+//  Created by Vitor Leonardi on 13/12/19.
+//  Copyright © 2019 Vitor. All rights reserved.
+//
+
+import Foundation
+
+struct Temperature: Equatable {
+    let feelsLike: Float
+    let humidity: Int
+    let pressure: Int
+    let temp: Float
+    let tempMax: Float
+    let tempMin: Float
+
+    init?(with dictionary: JSONDictionary) {
+        let mainDict = dictionary["main"] as? JSONDictionary
+        guard let feels_like = mainDict?.float(forKey: "feels_like") else { return nil }
+        guard let humidity = mainDict?.int(forKey: "humidity") else { return nil }
+        guard let pressure = mainDict?.int(forKey: "pressure") else { return nil }
+        guard let temp = mainDict?.float(forKey: "temp") else { return nil }
+        guard let temp_max = mainDict?.float(forKey: "temp_max") else { return nil }
+        guard let temp_min = mainDict?.float(forKey: "temp_min") else { return nil }
+
+        self.feelsLike = feels_like
+        self.humidity = humidity
+        self.pressure = pressure
+        self.temp = temp
+        self.tempMax = temp_max
+        self.tempMin = temp_min
+    }
+}
