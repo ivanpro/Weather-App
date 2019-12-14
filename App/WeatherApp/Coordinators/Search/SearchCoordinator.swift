3f06ca598a5115f9c423fad68267179f0fe41305
Vitor Lopes Leonardi <vitor.leonardi@theiconic.com.au>
2019-12-14 19:52:12 +1100

    Implement searchCoordinator
    Add UIViewController extension to hold basic struct with UI details

---
+++ App/WeatherApp/Coordinators/Search/SearchCoordinator.swift
@@ -0,0 +1,27 @@
+//
+//  SearchCoordinator.swift
+//  WeatherApp
+//
+//  Created by Vitor Leonardi on 14/12/19.
+//  Copyright © 2019 Vitor. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol SearchCoordinatorInterface: Coordinator {}
+
+final class SearchCoordinator: SearchCoordinatorInterface {
+    var navigationController: UINavigationController
+    var viewModel: SearchViewModelInterface
+
+    init(navigationController: UINavigationController, viewModel: SearchViewModelInterface) {
+        self.navigationController = navigationController
+        self.viewModel = viewModel
+    }
+
+    func start() {
+        let viewController = SearchViewController(viewModel: viewModel)
+        navigationController.pushViewController(viewController, animated: true)
+    }
+}
---
+++ Common/Extensions/UIViewController.swift
@@ -0,0 +1,16 @@
+//
+//  UIViewController.swift
+//  WeatherApp
+//
+//  Created by Vitor Leonardi on 14/12/19.
+//  Copyright © 2019 Vitor. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    struct Dimensions {
+        static let margin: CGFloat = 16.0
+    }
+}
