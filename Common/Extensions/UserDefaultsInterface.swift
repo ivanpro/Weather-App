d7304161471612fd56687d45a108ea0f3282e53b
Vitor Lopes Leonardi <vitor.leonardi@theiconic.com.au>
2019-12-15 13:38:54 +1100

    Add UserDefaultsInterface so we can easily mock it and test

---
+++ Common/Extensions/UserDefaultsInterface.swift
@@ -0,0 +1,16 @@
+//
+//  UserDefaultsInterface.swift
+//  WeatherApp
+//
+//  Created by Vitor Leonardi on 15/12/19.
+//  Copyright © 2019 Vitor. All rights reserved.
+//
+
+import Foundation
+
+public protocol UserDefaultsInterface {
+    func set(_ value: Any?, forKey defaultName: String)
+    func array(forKey defaultName: String) -> [Any]?
+}
+
+extension UserDefaults: UserDefaultsInterface {}
